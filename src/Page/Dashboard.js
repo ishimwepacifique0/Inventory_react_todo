bb4fed9fc4a88d78150eaec900b34d63c2487d68
ishimwepacifique0 <ishimwepacifique0@gmail.com>
2023-03-13 08:14:27 -0400

    add dashboard

--- src/Page/Dashboard.js
+++ src/Page/Dashboard.js
@@ -59,68 +59,31 @@ function Dashboard(props) {
         <>
             <div className='container'>
             <div className='row'>
-                <div className='col-xl-3 colo-md-6 mb-4'>
-                    <div className='card border-left-warning shadow h-100 py-2 bg-primary'>
-                        <div className='card-body'>
-                            <div className='row no-gutters align-items-center'>
-                                <div className='col mr-2'>
-                                    <div className='text-xs font-weight-bold text-white text-uppercase mb-1'>
-                                        Pending the Invoice
-                                        <div className='h5 mb-0 font-weight-bold text-gray-800'>
+                                        <div className='col-sm-4 bg-primary rounded py-3 mx-2 my-2'>
+                                            <div className='text-white font-bold'>Pending invoice</div>
                                                 {
                                                     countdata.length
                                                 }
                                         </div>
-                                    </div>
-                                    <div className='col-auto'>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-xl-3 colo-md-6 mb-4'>
-                    <div className='card border-left-warning shadow h-100 py-2 bg-secondary'>
-                        <div className='card-body'>
-                            <div className='row no-gutters align-items-center'>
-                                <div className='col mr-2'>
-                                    <div className='text-xs font-weight-bold text-white text-uppercase mb-1'>
-                                        Pending the Item
-                                        <div className='h5 mb-0 font-weight-bold text-gray-800'>
+                                        <div className='col-sm-4 bg-success rounded py-3 mx-2 my-2'>
+                                        <div className='text-white font-bold'>Pending item</div>
+
+
                                         {
                                                     countdataitem.length
                                                 }
                                         </div>
-                                    </div>
-                                    <div className='col-auto'>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-xl-3 colo-md-6 mb-4'>
-                    <div className='card border-left-warning shadow h-100 py-2 bg-success'>
-                        <div className='card-body'>
-                            <div className='row no-gutters align-items-center'>
-                                <div className='col mr-2'>
-                                    <div className='text-xs font-weight-bold text-white text-uppercase mb-1'>
-                                        Pending the Customer
-                                        <div className='h5 mb-0 font-weight-bold text-gray-800'>
+    
+                                        <div className='col-sm-4 bg-warning rounded py-3 mx-2 my-2'>
+                                        <div className='text-white font-bold'>Pending customers</div>
+
                                         {
                                                     countdata.length
                                                 }
                                         </div>
-                                    </div>
-                                    <div className='col-auto'>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+                                        </div>
+                                        </div>
     </>
     );
 }
